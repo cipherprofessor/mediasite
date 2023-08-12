2736339f260ecdecd6970c50b5851c6ecf3c47ae
Cipher Professor <mohsinmanzoor1913@gmail.com>
2023-08-12 21:54:01 +0530

    First deployment

---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Tech Side",
+    content:
+      "We will help you to get the tech side of your business by proving you mind blowing website and all the other you need",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Content Creation",
+    content:
+      "Don't worry we are hear to make amazing content which you can show to your customers",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Social Media Marketing",
+    content:
+      "Are you wooried about how to reach and grab the customers. We are here to help you with all the advertising",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Paisa Hei Paisa Chapna Hai Apun Ko",
+    name: "Syed Khan",
+    title: "Founder & CEO",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "I Am A Software Developer And My Passion Is To Always Learn New Technologies And Make Problem Solving Products.  ",
+    name: "Mohsin Manzoor Bhat",
+    title: "Co-Founder & CTO",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Content Creator Hou And Phadu Gamer",
+    name: "Ussaid Bin Abid",
+    title: "Co-Founder & CFO",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "10+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "20+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$1M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
